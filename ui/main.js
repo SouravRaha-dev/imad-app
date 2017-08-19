6183fdfccc8180435dd3e6f0be85024739bf6c37
Sourav Raha <sourav.raha099@gmail.com>
2017-08-19 20:26:33 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,12 @@
 console.log('Loaded!');
 var element = document.getElementById('main-text');
 element.innerHTML='New Value';
+var img = document.getElementById('madi');
+function moveRight(){
+    var marginLeft = 0;
+    marginLeft += 1;
+    img.style.marginLeft = marginLeft+'px';
+}
+img.onclick = function() {
+    var interval = setInterval(moveRight,50);
+};
