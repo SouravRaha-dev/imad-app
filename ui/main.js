66002da73448ad5afeaf14c6214cf25138ffb337
Sourav Raha <sourav.raha099@gmail.com>
2017-08-21 09:58:36 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -13,3 +13,20 @@ button.onclick = function() {
         request.open('GET','http://souravraha099.imad.hasura-app.io/counter',true);
         request.send(null);
 };
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+var submit = document.getElementById('submit_btn');
+submit.onclick = function () {
+    var request = new XMLHttpRequest();
+        request.onreadystatechange = function () {
+            if(request.readyState === XMLHttpRequest.DONE) {
+                if(request.status === 200) {
+                    var counter = request.responseText;
+                    var span = document.getElementById('count');
+                    span.innerHTML = counter.toString();
+                }
+            }
+        };
+        request.open('GET','http://souravraha099.imad.hasura-app.io/counter',true);
+        request.send(null);
+}
