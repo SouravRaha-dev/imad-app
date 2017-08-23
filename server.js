1e6108fd3c0aa111c1584d5a32702ca2f542f9f3
Sourav Raha <sourav.raha099@gmail.com>
2017-08-24 01:14:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-
+var Pool = require('pg').Pool;
 var app = express();
 app.use(morgan('combined'));
-
+var config = {
+  user: 'souravraha099',
+  database: 'souravraha099',
+  host: 'db.imad.hasura-app.io',
+  port: '5432',
+  password: process.env.DB_PASSWORD
+};
 var articles={
     'article-one':{
         title: 'Article One! Sourav Raha',
@@ -84,6 +90,15 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var pool = new Pool(config);
+app.get('/test-db', function (req, res) {
+    pool.query('SELECT * FROM test', function (err, result) {
+        if(err)
+            res.status(500).send(err.toString());
+        else
+            res.send(JSON.stringify(result.rows));
+    });
+});
 var counter = 0;
 app.get('/counter', function (req,res) {
     counter += 1;
