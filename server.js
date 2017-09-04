4bd62db582cd6443579598bc7de0942662403cbc
Sourav Raha <sourav.raha099@gmail.com>
2017-09-04 21:39:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -92,7 +92,8 @@ app.post('/login', function (req, res) {
                 var hashedPassword = hash(password,salt);
                 if(hashedPassword === dbString) {
                     req.session.auth = {userId: result.rows[0].id};
-                    res.send('Credentials correct!');
+                    res.setHeader('Content-Type', 'application/json');
+                    res.send(JSON.parse('{"message":"Credential Correct"}'));
                 }
                 else
                     res.send(403).send('username/password is invalid');
