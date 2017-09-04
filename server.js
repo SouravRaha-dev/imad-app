f39c378756b018365488abda81278ef6f02b6ea5
Sourav Raha <sourav.raha099@gmail.com>
2017-09-04 22:47:21 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,12 +6,14 @@ var crypto = require('crypto');
 var bodyParser = require('body-parser');
 var session = require('express-session');
 var app = express();
+
 app.use(morgan('combined'));
 app.use(bodyParser.json());
 app.use(session({
     secret: 'someRandomSecretValue',
     cookie: {maxAge: 1000*60*60*24*30}
 }));
+
 var config = {
   user: 'souravraha099',
   database: 'souravraha099',
@@ -19,6 +21,7 @@ var config = {
   port: '5432',
   password: process.env.DB_PASSWORD
 };
+
 function createTemplate(data){
     var title=data.title;
     var date=data.date;
@@ -54,17 +57,21 @@ function createTemplate(data){
     `;
     return htmlTemplate;
 }
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
 function hash (input, salt) {
     var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
     return ["pbkdf2", "10000", salt, hashed.toString('hex')].join('$');
 }
+
 app.get('/hash/:input', function (req, res) {
     var hashedString = hash(req.params.input, 'this-is-some-random-string');
     res.send(hashedString);
 });
+
 app.post('/create-user', function (req, res) {
     var username = req.body.username;
     var password = req.body.password;
@@ -77,6 +84,7 @@ app.post('/create-user', function (req, res) {
             res.send('User successfully created :' + username);
     });
 });
+
 app.post('/login', function (req, res) {
     var username = req.body.username;
     var password = req.body.password;
@@ -100,16 +108,19 @@ app.post('/login', function (req, res) {
         }
     });
 });
+
 app.get('/check-login', function (req, res) {
     if(req.session && req.session.auth && req.session.auth.userId)
         res.send('You are logged in: ' + req.session.auth.userId.toString());
     else
         res.send('You are not logged in');
 });
+
 app.get('/logout', function (req, res) {
     delete req.session.auth;
     res.send('Logged out');
 });
+
 var pool = new Pool(config);
 app.get('/test-db', function (req, res) {
     pool.query('SELECT * FROM test', function (err, result) {
@@ -119,17 +130,20 @@ app.get('/test-db', function (req, res) {
             res.send(JSON.stringify(result.rows));
     });
 });
+
 var counter = 0;
 app.get('/counter', function (req,res) {
     counter += 1;
     res.send(counter.toString());
 });
+
 var names=[];
 app.get('/submit-name', function (req,res) {
     var name = req.query.name;
     names.push(name);
     res.send(JSON.stringify(names));
 });
+
 app.get('/articles/:articleName', function (req,res) {
     pool.query("SELECT * FROM article WHERE title = $1", [req.params.articleName] , function (err, result) {
         if(err)
@@ -144,12 +158,15 @@ app.get('/articles/:articleName', function (req,res) {
         }
     });
 });
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
+
 app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
+
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
