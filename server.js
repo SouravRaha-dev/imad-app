5a2fbfb3b731981c840607ee41f4f0df69430623
Sourav Raha <sourav.raha099@gmail.com>
2017-09-05 12:48:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -81,7 +81,7 @@ app.post('/create-user', function (req, res) {
         if(err)
             res.status(500).send(err.toString());
         else
-            res.send(JSON.parse('{"message":"User successfully created :"}') + username);
+            res.send(JSON.parse('{"message":"User successfully created: ' + username + ' "}'));
     });
 });
 
